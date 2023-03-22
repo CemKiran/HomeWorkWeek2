3ef016bfb7c126927ae522f806b03779846117f9
cemkr <cemkr@XUAFEN>
2023-03-22 14:54:24 +0300

    Add project files.

---
+++ HomeWorkWeek2/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+ * Senaryo şu şekilde olacak;
+ * İnheritance kavramı için verilen ufak ödev için senaryom şu
+ * Karakter muhafız ve korunalar ile savaşacak
+ * 
+ * Karakterde şu olacak
+ * Silah
+ * Zırh
+ * Can
+ * Kritik
+ * Mermi
+ * Bomba
+ * Bu değerler savaş esnasında değişiklik gösterilmesi için gerekli kodlar yazılacak
+ * 
+ * Silahlar: Balta( hasarı sabit her zaman) , Kılıç(Hasarı her round farklı) , Bıçak(Kritk vurabilir)
+ * 
+ * Muhafızlar şu şekilde olacak:
+ * Round başlarındaki koruma kalkanının her round aynı sürede bitmesi
+ * Karakterlerin muhafızların zırhlarını delebilme ihtimali yüksek
+ * Son roundlarda zırhlarının ekstra düşüş yaşanması
+ * Yüksek seviye karakterlerin yüksek kritik hasar vererek muhafızlara tek atması
+ * Orta seviye karakterlerin ellerindeki silahları yükseltmeleri ve kendilerine eklenti ile daha fazla hasar verebilmesi.
+ * 
+ * Korumalar:
+ * 
+ * Orta seviye zırh ile oyuna başlayarak hasarların belli kısımlarını soğurmalarına sebep olur
+ * Yüksek seviye korumaların karakterler tarafından kritik yeme şansının az olması
+ * Korumanın kullandığı silahların ekstra olarak hasar verebilmesi 
+ * 
+ * 
+ * Hem bilgisayar hemde online bir şekilde savaş yapılabilecek.
+ */
